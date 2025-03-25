fe767f1c2d4cf5de380a0984019b551a935c0543
ASKurgan <infopret@yandex.ru>
2025-03-25 16:34:52 +0300

    Добавление проекта который сделаем nugetпакетом.

---
+++ ClassLibrary4/ClassLibrary4/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary4
+{
+    public class Class1
+    {
+        public string SayHello()
+        {
+            return "Hello! I am Class1 from ClassLibrary4.";
+        }
+
+    }
+}
