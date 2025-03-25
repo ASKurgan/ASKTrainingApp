c15c2985944127b3382417779a70e6076d43d20c
ASKurgan <infopret@yandex.ru>
2025-03-25 15:14:40 +0300

    Added projects.

---
+++ ASKConsoleApp/ASKConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(new ClassLibrary1.Class1().SayHello());
+
+Console.WriteLine(new ClassLibrary2.Class1().SayHello());
---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public string SayHello()
+        {
+            return "Hello! I am Class1 from ClassLibrary2.";
+        }
+    }
+}
